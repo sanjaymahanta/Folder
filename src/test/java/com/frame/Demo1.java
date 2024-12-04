7bf878f9dd77dc140a0f4f3cbdac3d5dbf63f885
Dell <Dell@192.168.56.1>
2024-12-04 18:47:37 +0530

    First Commit Folder

---
+++ src/test/java/com/frame/Demo1.java
@@ -0,0 +1,38 @@
+package com.frame;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	WebDriver driver;
+
+	@BeforeClass
+	public void setup() {
+
+		System.setProperty("webdriver.edge.driver", "D:/msedgedriver.exe");
+		driver = new EdgeDriver();
+	}
+
+	@Test
+	public void testFrameYoutube() {
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//a[@class='gb_A']")).click();
+
+		WebElement element = driver.findElement(By.name("app"));
+		driver.switchTo().frame(element);
+
+		driver.findElement(By.xpath("//span[text()='YouTube']")).click();
+
+	}
+
+}
